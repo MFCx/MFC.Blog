23a38ff1a110bdbcfd4dc2d10c0d005cdfe0c418
Muhammed Fatih CEYLAN <Yl1930138013@ogr.sdu.edu.tr>
2021-04-30 12:03:11 +0300

    MicrosoftIoC =>CustomIoCExtension Class added for dependencies.

---
+++ MFC.Blog.Business/Containers/MicrosoftIoC/CustomIoCExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MFC.Blog.Business.Concrete;
+using MFC.Blog.Business.Interfaces;
+using MFC.Blog.DataAccess.Concrete.EntityFrameworkCore.Repositories;
+using MFC.Blog.DataAccess.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MFC.Blog.Business.Containers.MicrosoftIoC
+{
+    public static class CustomIoCExtension
+    {
+        public static void AddDependencies(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericDal<>),typeof(EfGenericRepository<>));
+            services.AddScoped(typeof(IGenericService<>),typeof(GenericManager<>));
+        }
+
+    }
+}
